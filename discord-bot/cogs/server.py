a0c2d3bab5427a370fcb4faef326ffe8d01590e4
Ashley Eckard <doodlebunnyhops@gmail.com>
2024-10-13 05:08:30 -0400

    rename cmd to zmod

--- discord-bot/cogs/server.py
+++ discord-bot/cogs/server.py
@@ -18,7 +18,7 @@ class RoleAccess(commands.Cog):
         self.bot = bot
 
     # Top-level group
-    server = app_commands.Group(name="server", description="Moderator Commands")
+    server = app_commands.Group(name="zmod", description="Moderator Commands")
 
     server.add_command(set_group)
     server.add_command(get_group)
